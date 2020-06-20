d606039dd0ba32a5344e65e6b606389fc321d739
Sindia Höller <sindia.hoeller@outlook.com>
2020-06-20 21:25:27 +0200

    Implemented Windows Updater & Uninstall %AppData%

--- SpiritTime.Frontend/Infrastructure/ElectronConfig/ElectronUpdater.cs
+++ SpiritTime.Frontend/Infrastructure/ElectronConfig/ElectronUpdater.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Blazorise;
 using ElectronNET.API;
 
 namespace SpiritTime.Frontend.Infrastructure.ElectronConfig
@@ -14,17 +15,17 @@ namespace SpiritTime.Frontend.Infrastructure.ElectronConfig
                 Electron.AutoUpdater.OnCheckingForUpdate  += async () => await Electron.Dialog.ShowMessageBoxAsync("Checking for Updates...");
                 Electron.AutoUpdater.OnUpdateNotAvailable += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update not available");
                 Electron.AutoUpdater.OnUpdateAvailable    += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update available" + info.Version);
-                // Electron.AutoUpdater.OnDownloadProgress += (info) =>
-                // {
-                //     var message1    = "Download speed: " + info.BytesPerSecond + "\n<br/>";
-                //     var message2    = "Downloaded " + info.Percent + "%"       + "\n<br/>";
-                //     var message3    = $"({info.Transferred}/{info.Total})"     + "\n<br/>";
-                //     var message4    = "Progress: " + info.Progress             + "\n<br/>";
-                //     var information = message1 + message2 + message3           + message4;
+                Electron.AutoUpdater.OnDownloadProgress += (info) =>
+                {
+                    var message1    = "Download speed: " + info.BytesPerSecond + "\n<br/>";
+                    var message2    = "Downloaded " + info.Percent + "%"       + "\n<br/>";
+                    var message3    = $"({info.Transferred}/{info.Total})"     + "\n<br/>";
+                    var message4    = "Progress: " + info.Progress             + "\n<br/>";
+                    var information = message1 + message2 + message3           + message4;
 
-                    // var mainWindow = Electron.WindowManager.BrowserWindows.First();
-                    // Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
-                // };
+                    var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                    Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+                };
                 Electron.AutoUpdater.OnUpdateDownloaded += async (info) => await Electron.Dialog.ShowMessageBoxAsync("Update complete!" + info.Version);
 
 
@@ -35,15 +36,21 @@ namespace SpiritTime.Frontend.Infrastructure.ElectronConfig
             }
         }
 
-        public static async Task CheckVersion()
+        public static async Task Check()
+        {
+            await Electron.AutoUpdater.CheckForUpdatesAndNotifyAsync();
+        }
+
+        public static async Task<string> CheckVersion()
         {
             var    currentVersion    = await Electron.App.GetVersionAsync();
             var    updateCheckResult = await Electron.AutoUpdater.CheckForUpdatesAndNotifyAsync();
             var    availableVersion  = updateCheckResult.UpdateInfo.Version;
             string information       = $"Current version: {currentVersion} - available version: {availableVersion}";
 
-            var mainWindow = Electron.WindowManager.BrowserWindows.First();
-            Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
+            return information;
+            // var mainWindow = Electron.WindowManager.BrowserWindows.First();
+            // Electron.IpcMain.Send(mainWindow, "auto-update-reply", information);
         }
     }
 }
